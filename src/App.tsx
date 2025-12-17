63d140cde12321a1386b783ed34745871d218b8c
vivek <igvivekr@gmail.com>
2025-12-18 00:47:40 +0530

    UI New & Updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import Foods from './Components/Foods'
+import SectionB from './Components/SectionB'
+import SectionC from './Components/SectionC'
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <Foods/>
+      <SectionB/>
+      <SectionC/>
     </div>
   )
 }
