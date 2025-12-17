658179d6e4ba8d159fd83fea548769cfb835b8f7
vivek <igvivekr@gmail.com>
2025-12-17 23:04:53 +0530

    UI fixes 2

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
             <span className='ml-3'>Current Location </span>
             <span className='font-bold ml-1'>M.P Nagar Bhopal Ward No.16</span>
         </div>
-        <div className='flex items-center'>
+        <div className='flex items-center px-4'>
           <i className="ri-search-line text-amber-500 text-2xl mr-3 font-bold"></i>
           <input className='w-2/4 border border-zinc-800 text-black px-2 py-1 rounded focus:border-zinc-900 focus:right-2' placeholder='Search Any Food..' type="text" name="searchfood" id="searchbar"/>
           <button className='px-4 py-1 text-amber-500 font-bold ml-6 shadow-[0px_13px_26px_-4px] rounded shadow-amber-500 cursor-pointer transition ease hover:scale-105'><i className="ri-user-fill fill-amber-400 mr-1"></i>Login</button>
